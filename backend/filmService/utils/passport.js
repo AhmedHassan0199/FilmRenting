4b4adc403c739ef4cb1f1a3eda3725816557ecef
muhadel <mohamed.adel@barqsystems.com>
2020-09-01 09:56:06 +0200

    Fix passport problem

---
+++ backend/filmService/utils/passport.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { jwtSecretKey } = require('../utils/keys');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwtSecretKey;
+
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    console.log('PAYLOAD:', jwt_payload);
+    mongoose.model('filmData').findOne({ _id: jwt_payload.id }, function (err, user) {
+      console.log('ERROR : ' + err);
+      console.log('USER_FOUND : ' + user);
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
+
+//Authentication from verifing the token and returning a user if token valid
+const isAuthenticated = passport.authenticate('jwt', { session: false });
+
+module.exports = { isAuthenticated };
